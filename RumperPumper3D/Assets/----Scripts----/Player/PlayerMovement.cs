d9b5c9ca4626c950468c52f1da72fa1d92d950bd
Kostya778899 <89563229+Kostya778899@users.noreply.github.com>
2022-02-16 10:04:45 +0500

    Сделал анимации оленю.

---
+++ RumperPumper3D/Assets/----Scripts----/Player/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5f;
+
+
+    private void Start()
+    {
+        
+    }
+}
